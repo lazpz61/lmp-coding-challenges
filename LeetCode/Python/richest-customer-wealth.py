bbe3af78652bb1315c17e9c47b3021dc6eb41db2
LazaroMPerez <lazpz61@gmail.com>
2021-05-28 19:28:26 -0400

    python problem

--- LeetCode/Python/richest-customer-wealth.py
+++ LeetCode/Python/richest-customer-wealth.py
@@ -5,4 +5,15 @@ the amount of money the i​​​​​​​​​​​th​​​​ custome
 A customer's wealth is the amount of money they have in all their bank accounts. 
 The richest customer is the customer that has the maximum wealth
 """
+class Solution:
+  def maximumWealth (self, accounts):
+      res, curr= 0, 0
+      for w in accounts:
+          for c in w:
+            curr+=c
+            res=max(res,curr)
+          curr=0
+      return res
 
+
+maximumWealth([1,2,3],[3,4,5],[3,3,3])
